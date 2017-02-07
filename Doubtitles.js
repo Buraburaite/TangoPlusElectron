f16e70deac0b7e8c10f5e464ac2d8a2d4d243c2a
Javier <jvrcueto@outlook.com>
2017-02-07 12:16:42 -0500

    atom did something weird, fear-save

--- Doubtitles.js
+++ Doubtitles.js
@@ -1,19 +1,34 @@
 
 class Slide {
-  constructor(text, startTime, endTime) { //Pre: times are in milliseconds
-    this.text = text;
-    this.startTime = this.mark = startTime;
-    this.endTime = endTime;
+  constructor(text, startTime, endTime = startTime + 1) { //Pre: times are in milliseconds
+    this.text      = text;
+    this.startTime = startTime;
+    this.endTime   = endTime;
   }
+
 }
+
+/*===
+'Slide.mark' is another name for 'Slide.startTime'. Conceptually, slides can be
+thought as a sequence of startTimes. For instance, if two slides overlap, that
+is presented as three slides: Slide A -> Slide A & B -> Slide B. That is, the
+overlap is considered its own slide, which simplifies overlapping slides into
+a video display issue. Therefore, one should use Slide.mark over Slide.startTime,
+unless you are specifically trying to differentiate the starting time from the
+ending time, in which case you should use Slide.startTime in conjunction with
+Slide.endTime, although functionally there is no difference.
+===*/
+Object.defineProperty(Slide.prototype, 'mark', {
+  get: function()        { return this.startTime; },
+  set: function(newTime) { this.startTime = newTime; }
+});
 
 
 class Doubtitles {
 
   constructor (srt) {
 
-    this.slides     = this.parseSrt(srt);
-    this.slideMarks = Array.from(this.slides.keys());
+    this.slides = this.parseSrt(srt);
   }
 
   parseSrt(filename) {
@@ -28,16 +43,20 @@ class Doubtitles {
     let subs = data.split("\r\n");
     subs = subs.filter(function isNotAnEmptyString(value) { return value !== ""; });
 
-    let slides = new Map();
+    let slides = [];
 
-    slides.set(0, "");
+    slides.push(new Slide('', 0));
+    let text, startTime, endTime;
     for (let i = 0; i < subs.length; i++){
       if (subs[i].includes("-->")){
-        slides.set(srtToMilliseconds(subs[i].slice(0,12)), subs[i + 1]);
-        slides.set(srtToMilliseconds(subs[i].slice(17)), "");
+        text      = subs[i + 1];
+        startTime = srtToMilliseconds(subs[i].slice(0,12));
+        endTime   = srtToMilliseconds(subs[i].slice(17,29));
+        slides.push(new Slide(text, startTime, endTime));
+        slides.push(new Slide(text, endTime)); //TODO: soon to-be deprecated
       }
     }
-    slides.set(Number.MAX_SAFE_INTEGER, ""); //I forget why I needed this line
+    slides.push(new Slide('', Number.MAX_SAFE_INTEGER - 1)); //I forget why I needed this line
 
     function srtToMilliseconds(timeString){
       timeString = timeString.replace(',','');
@@ -50,6 +69,9 @@ class Doubtitles {
       return ms;
     }
 
+    slides.sort(function sortByStartTime(a,b) {
+      return a.startTime - b.startTime;
+    });
     return slides;
 
   }
