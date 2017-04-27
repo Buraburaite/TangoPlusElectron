aa6d6274b58a89170b5bcba65768649680dd9bc3
Javier <jvrcueto@outlook.com>
2017-04-26 23:08:37 -0400

    working on resizing code for window, seeing some weird flipping behavior with the height of the video element

--- scripts/video-events.js
+++ scripts/video-events.js
@@ -1,14 +1,29 @@
 const jWin = $(window);
 const jVideo = $(myPlayer.videoEl);
 
+const scaleWithVWidth  = (jel) => jel.width ('100vw').height('auto');
+const scaleWithVHeight = (jel) => jel.height('100vh').width('auto');
+
 jWin.resize((e) => {
-  if (jWin.width() >  jWin.height()) {
-    jVideo
-    .width('auto')
-    .height('92vh');
-  } else {
-    jVideo
-    .width('92vw')
-    .height('auto');
+  const vwidth = jWin.width();
+  // console.log(jVideo.width());
+  if (vwidth >  jWin.height()) { //if window is wide
+    if (jVideo.width() > vwidth) { //but video is too wide to fit
+      scaleWithVWidth(jVideo); //scale to fit inside
+      console.log('here1');
+    }
+    else {
+      scaleWithVHeight(jVideo); //else, scale with the height
+      console.log('here2');
+    }
+  }
+  else { //if window is tall or square
+    scaleWithVWidth(jVideo); //scale with the width
+    console.log('here3');
   }
 });
+
+
+//window is wide and video fits: scaleWithVHeight
+//window is wide but video clipped: scaleWithVWidth
+//window is tall and video fits: scaleWithVWidth
