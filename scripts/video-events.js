2ba51d303fcf03b7294b73b4128bf96ff31f656c
Javier <jvrcueto@outlook.com>
2017-04-29 23:04:21 -0400

    screen resizing issue if confusing, but it seems like it consistently flips back and forth from correct to incorrect. Also, making it asynchronous did not resolve issue, so it seems to be a logic problem, not a timing one

--- scripts/video-events.js
+++ scripts/video-events.js
@@ -2,28 +2,39 @@ const jWin = $(window);
 const jVideo = $(myPlayer.videoEl);
 
 const scaleWithVWidth  = (jel) => jel.width ('100vw').height('auto');
-const scaleWithVHeight = (jel) => jel.height('100vh').width('auto');
+const scaleWithVHeight = (jel) => jel.width('auto').height('100vh');
 
-jWin.resize((e) => {
-  const vwidth = jWin.width();
-  // console.log(jVideo.width());
-  if (vwidth >  jWin.height()) { //if window is wide
-    if (jVideo.width() > vwidth) { //but video is too wide to fit
-      scaleWithVWidth(jVideo); //scale to fit inside
+const setVidSize = () => {
+  const winWidth = jWin.width();
+  const winHeight = jWin.height();
+  if (winWidth >  winHeight) { //if window is wide
+    if (jVideo.width() > winWidth) {
+      scaleWithVWidth(jVideo);
       console.log('here1');
     }
     else {
-      scaleWithVHeight(jVideo); //else, scale with the height
+      scaleWithVHeight(jVideo);
       console.log('here2');
     }
   }
   else { //if window is tall or square
-    scaleWithVWidth(jVideo); //scale with the width
+    scaleWithVWidth(jVideo);
     console.log('here3');
   }
+};
+
+let winTimer = setTimeout(setVidSize, 1000);
+
+
+//We know window always starts off wide
+jWin.resize((e) => {
+  clearTimeout(winTimer);
+  winTimer = setTimeout(setVidSize, 1000);
 });
 
 
-//window is wide and video fits: scaleWithVHeight
+//window is wide and video fits:    scaleWithVHeight
 //window is wide but video clipped: scaleWithVWidth
-//window is tall and video fits: scaleWithVWidth
+//window is tall and video fits:    scaleWithVWidth
+
+//100vw & autoh => autow & autoh => autow & 100vh
