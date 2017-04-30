31f6b53829e599dbe366ac0c58a8da7509bc37dc
Javier <jvrcueto@outlook.com>
2017-04-29 23:43:11 -0400

    window resizing is finally perfect.

--- scripts/video-events.js
+++ scripts/video-events.js
@@ -1,43 +1,26 @@
 const jWin = $(window);
 const jVideo = $(myPlayer.videoEl);
 
-const scaleWithVWidth  = (jel) => { jel.width ('100vw'); jel.height('auto'); };
-const scaleWithVHeight = (jel) => { jel.width('auto'); jel.height('100vh'); };
+const scaleWithVWidth  = (jel) => jel.width ('100vw').height('auto');
+const scaleWithVHeight = (jel) => jel.width('auto').height('100vh');
+const videoAspectRatio = () => jVideo.width() / jVideo.height();
 
-const setVidSize = () => {
-  console.log(jVideo.width() / jVideo.height());
+//Strategy
+//window is wide and video fits:    scaleWithVHeight
+//window is wide but video clipped: scaleWithVWidth
+//window is tall and video fits:    scaleWithVWidth
+jWin.resize(() => {
   const winWidth = jWin.width();
   const winHeight = jWin.height();
   if (winWidth >  winHeight) { //if window is wide
-    if (jVideo.width() / jVideo.height() > winWidth) {
-      // console.log(jVideo.width(), winWidth);
+    if (videoAspectRatio() * winHeight > winWidth) { //if video would be too wide to fit
       scaleWithVWidth(jVideo);
-      console.log('here1');
     }
     else {
-      // console.log(jVideo.width(), winWidth);
       scaleWithVHeight(jVideo);
-      console.log('here2');
     }
   }
   else { //if window is tall or square
     scaleWithVWidth(jVideo);
-    console.log('here3');
   }
-};
-
-let winTimer = setTimeout(setVidSize, 1000);
-
-
-//We know window always starts off wide
-jWin.resize((e) => {
-  clearTimeout(winTimer);
-  winTimer = setTimeout(setVidSize, 1000);
 });
-
-
-//window is wide and video fits:    scaleWithVHeight
-//window is wide but video clipped: scaleWithVWidth
-//window is tall and video fits:    scaleWithVWidth
-
-//100vw & autoh => autow & autoh => autow & 100vh
