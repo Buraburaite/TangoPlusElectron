8f571762e6fe8f60179b8cf78801c263160046e1
Javier <jvrcueto@outlook.com>
2017-04-29 23:10:46 -0400

    figured it out, need a different way of checking if the video would fit once scaled with the window's height

--- scripts/video-events.js
+++ scripts/video-events.js
@@ -1,18 +1,20 @@
 const jWin = $(window);
 const jVideo = $(myPlayer.videoEl);
 
-const scaleWithVWidth  = (jel) => jel.width ('100vw').height('auto');
-const scaleWithVHeight = (jel) => jel.width('auto').height('100vh');
+const scaleWithVWidth  = (jel) => { jel.width ('100vw'); jel.height('auto'); };
+const scaleWithVHeight = (jel) => { jel.width('auto'); jel.height('100vh'); };
 
 const setVidSize = () => {
   const winWidth = jWin.width();
   const winHeight = jWin.height();
   if (winWidth >  winHeight) { //if window is wide
-    if (jVideo.width() > winWidth) {
+    if (jVideo.width() >= winWidth) { // <==This is a bad measurement of what you want
+      console.log(jVideo.width(), winWidth);
       scaleWithVWidth(jVideo);
       console.log('here1');
     }
     else {
+      console.log(jVideo.width(), winWidth);
       scaleWithVHeight(jVideo);
       console.log('here2');
     }
