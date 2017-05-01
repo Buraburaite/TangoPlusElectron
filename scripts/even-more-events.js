bc1a04a68aa3584f1ac98b1228b47c350a8d460d
Javier <jvrcueto@outlook.com>
2017-04-30 23:24:01 -0400

    tried to make handle scale relatively, but failed. Left a note for the future

--- scripts/even-more-events.js
+++ scripts/even-more-events.js
@@ -3,24 +3,23 @@ const jHandle = myPlayer.jHandle;
 
 const divHeight = $('#progress-div').height();
 
-const growHandle = () => {
-  jHandle
-  .width(divHeight)
-  .height(divHeight);
-};
-const shrinkHandle = () => {
-  jHandle
-  .width(divHeight / 2)
-  .height(divHeight / 2);
-};
-
 const growBar = () => {
   jProgress.height('100%');
-  growHandle();
+    jHandle
+    .width(divHeight)
+    .height(divHeight);
 };
 const shrinkBar = () => {
   jProgress.height('50%');
-  shrinkHandle();
+    jHandle
+    .width(divHeight / 1.5)
+    .height(divHeight / 1.5);
 };
 
 jProgress.hover(growBar, shrinkBar);
+
+shrinkBar();
+
+//Known issue:
+//The size of the handle is not relative, so at extreme window sizes
+//it looks strange
--- scripts/video-events.js
+++ scripts/video-events.js
@@ -20,6 +20,8 @@ const resizeVideoToFitWindow = () => {
   else {
     scaleWithVWidth(jVideo);
   }
+
+  shrinkBar();
 };
 
 jWin.resize(resizeVideoToFitWindow);
