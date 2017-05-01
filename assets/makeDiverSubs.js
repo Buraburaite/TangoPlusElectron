a175453e0d596318049ee1cb0ded7da7f9c6cad2
Javier <jvrcueto@outlook.com>
2017-05-01 00:04:35 -0400

    converting old python file to JavaScript, in order to generate a file with the proper timestamps

--- assets/makeDiverSubs.js
+++ assets/makeDiverSubs.js
@@ -1,9 +1,11 @@
-with open("diver numbers.txt", "r") as f:
-    nums = f.read()
+const fs = require('fs');
+let nums = fs.readFileSync(__dirname + '/diver numbers.txt', 'utf-8');
+nums = nums
+.split('\r\n')
+.map((n) => parseInt(n));
 
-nums = [int(num) for num in nums.split("\n")]
-
-lyrics=["あの水平線が遠ざかっていく",
+const lyrics =
+["あの水平線が遠ざかっていく",
 "青すぎた空には明日すら描けなくて",
 "息もできないくらい澱んだ人の群れ",
 "僕はいつからここに潜り込んだんだ？",
@@ -28,33 +30,31 @@ lyrics=["あの水平線が遠ざかっていく",
 "生きているんだって確かめたいならそう",
 "深い海底を目指してもう一度",
 "息をしてみて",
-"ただの幸せに気づいたらもう二度と溺れないよ"]
+"ただの幸せに気づいたらもう二度と溺れないよ"];
 
-def twoSpaces(n):
-    n = str(n)
-    if len(n) == 1:
-        return "0" + n
-    else:
-        return n
-        
+const twoSpaces = (n) => {
+  if (n.length === 1) { return '0' + n; }
+  else                { return ''  + n; }
+};
 
-for i in range(len(nums)):
-    r = nums[i]
-    r %= 3600000
-    hours = nums[i] - r
-    r %= 60000
-    minutes = nums[i] - hours - r
-    
-    hours = twoSpaces(hours // 3600000)
-    minutes = twoSpaces(minutes // 60000)
-    r = str(r)
-    
-    nums[i] = hours + ":" + minutes + ":" + r[0:2] + "," + r[2:]
 
-nums = [str(i//2 + 1) + "\n" + nums[i] + " --> " + nums[i+1] + "\n" + lyrics[i//2] + "\n\n" for i in range(0, len(nums)) if i % 2 == 0]
-nums = "".join(nums)
-
-print(nums)
-
-with open("diver subs.srt", "w", encoding="utf-16") as f:
-    f.write(nums)
+// for i in range(len(nums)):
+//     r = nums[i]
+//     r %= 3600000
+//     hours = nums[i] - r
+//     r %= 60000
+//     minutes = nums[i] - hours - r
+//
+//     hours = twoSpaces(hours // 3600000)
+//     minutes = twoSpaces(minutes // 60000)
+//     r = str(r)
+//
+//     nums[i] = hours + ":" + minutes + ":" + r[0:2] + "," + r[2:]
+//
+// nums = [str(i//2 + 1) + "\n" + nums[i] + " --> " + nums[i+1] + "\n" + lyrics[i//2] + "\n\n" for i in range(0, len(nums)) if i % 2 == 0]
+// nums = "".join(nums)
+//
+// print(nums)
+//
+// with open("diver subs.srt", "w", encoding="utf-16") as f:
+//     f.write(nums)
