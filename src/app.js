240e6e4c60f9e6774b53f87e0dd14a55742b755f
Javier <jvrcueto@outlook.com>
2017-10-13 12:58:48 -0400

    loading a new video now works, although code needs to be relocated

--- src/app.js
+++ src/app.js
@@ -1,2 +1,17 @@
+const electron = require('electron');
+const dialog = electron.remote.dialog;
+
 const Player = require('./Player/Player.js');
 const player = new Player('#Player');
+
+const openFile = () => {
+  dialog.showOpenDialog(
+    {
+      filters: [
+        { name: 'video (*.mp4)', extensions: ['mp4'] },
+        { name: 'audio (*.mp3)', extensions: ['mp3'] }
+      ]
+    },
+    (fileNames) => { $('#Player').attr('src', fileNames[0]); }
+  );
+};
