7429195d26972d16025c08be3a4ed07f246cc563
Javier <jvrcueto@outlook.com>
2017-10-16 20:35:23 -0400

    added play-pause-btn, wired it up

--- src/Player/components/Controls.js
+++ src/Player/components/Controls.js
@@ -1,11 +1,13 @@
 const askForSourceFactory = require('../factories/askForSource.js');
 const fullscreenFactory = require('../factories/fullscreen.js');
+const playPauseFactory = require('../factories/playPause.js');
 
 class Controls {
 
   constructor(tags) {
 
     // various buttons:CLICK
+    $(tags.playPauseTag).click( playPauseFactory(tags));
     $(tags.loadTag).click(      askForSourceFactory(tags));
     $(tags.fullscreenTag).click(fullscreenFactory(tags));
   }
