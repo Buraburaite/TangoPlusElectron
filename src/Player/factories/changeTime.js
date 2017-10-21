1daa12a451c3fad718eaf6c770c4cb0ce3b08910
Javier <jvrcueto@outlook.com>
2017-10-20 21:41:05 -0400

    remove chageTime, unneeded for now

--- src/Player/factories/changeTime.js
+++
@@ -1,9 +0,0 @@
-module.exports = (tags, transformTime) => {
-  const videoEl = $(tags.video).get(0);
-
-  const changeTime = () => {
-    videoEl.currentTime = transformTime(videoEl.currentTime);
-  };
-
-  return changeTime;
-};
