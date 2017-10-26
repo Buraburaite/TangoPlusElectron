4b9216afab7d3b1248213f10d8abb0f0c4d51363
Javier <jvrcueto@outlook.com>
2017-10-26 17:54:59 -0400

    removed icons, we don't seem to need them yet

--- src/app.js
+++ src/app.js
@@ -1,25 +1,26 @@
 const Player = require('./Player/Player.js');
-const player = new Player(
-  {
-    player: '#Player',
-    skipRegion: '.skip',
-    skipBack: '#skip-back',
-    skipForward: '#skip-forward',
-    slide: '#Slide',
-    controls: '#Controls',
-    progress: '#Progress',
-    playPauseBtn: '#play-pause-btn',
-    muteBtn: '#mute-btn',
-    volumeSldr: '#volume-sldr',
-    timeBtn: '#time-btn',
-    cTime: '#current-time',
-    duration: '#duration',
-    autoReplayBtn: '#auto-replay-btn',
-    loadBtn: '#load-btn',
-    fullscreenBtn: '#fullscreen-btn',
-    video: '#Video',
-    videoContainer: '#video-container'
-  }
-);
+
+tags = {
+  player: '#Player',
+  skipRegion: '.skip',
+  skipBack: '#skip-back',
+  skipForward: '#skip-forward',
+  slide: '#Slide',
+  controls: '#Controls',
+  progress: '#Progress',
+  playPauseBtn: '#play-pause-btn',
+  muteBtn: '#mute-btn',
+  volumeSldr: '#volume-sldr',
+  timeBtn: '#time-btn',
+  cTime: '#current-time',
+  duration: '#duration',
+  autoReplayBtn: '#auto-replay-btn',
+  loadBtn: '#load-btn',
+  fullscreenBtn: '#fullscreen-btn',
+  video: '#Video',
+  videoContainer: '#video-container'
+};
+
+const player = new Player(tags);
 
 $('#Video').attr('src', 'assets/diver.mp4');
