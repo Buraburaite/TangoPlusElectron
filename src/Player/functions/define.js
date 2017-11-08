5c1b65ea956307df41ae460478815c3fe6e9a3db
Javier <jvrcueto@outlook.com>
2017-11-08 16:56:03 -0500

    added licensing for JMdict

--- src/Player/functions/define.js
+++ src/Player/functions/define.js
@@ -1,28 +1,25 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./src/vendor/JMdict.db');
 
 module.exports = (word) => {
+
+  const db = new sqlite3.Database('./src/vendor/JMdict/JMdict.db');
 
   const lookupWord = new Promise((resolve, reject) => {
 
     matchs = [];
 
     db.each(
-      'SELECT id, kanji, kana FROM jisho WHERE kanji = ?',
+      'SELECT * FROM jisho WHERE kanji = ?',
       word,
       (err, row) => { // foreach callback (this happens after all results are found)
 
         if (err) { reject(err); return; }
-
-        console.log('found something');
 
         matchs.push(row);
       },
       (err, rowCount) => { // completion callback
 
         if (err) { reject(err); return; }
-
-        console.log(rowCount, matchs);
 
         db.close();
 
