04590d55d60bca89e7cef424903498aca74152eb
Javier Cueto <javi@Yanets-MacBook-Pro.local>
2017-11-18 09:59:56 -0500

    window background color now black, improves user experience

--- app/electron-start.js
+++ app/electron-start.js
@@ -14,7 +14,11 @@ let win;
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 1200, height: 1000});
+  win = new BrowserWindow({
+    width: 1200,
+    height: 1000,
+    backgroundColor: '#000000'
+  });
 
   // and load the index.html of the app.
   win.loadURL(url.format({
