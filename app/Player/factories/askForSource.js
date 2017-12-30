9f583fc3a4160666362aa4c563f50e47d8e8af06
Javier Cueto <javi@Yanets-MacBook-Pro.local>
2017-12-29 21:26:10 -0500

    improvements to changngeSource and changeSubs

--- app/Player/factories/askForSource.js
+++ app/Player/factories/askForSource.js
@@ -2,13 +2,19 @@ const dialog = require('electron').remote.dialog;
 
 module.exports = (tags, services) => {
   const jVideo = $(tags.video);
+  const videoEl = jVideo.get(0);
 
-  const changeSource = (filepath) => {
-    jVideo.attr('src', filepath[0]);
+  const changeSource = (filepaths) => {
+
+    // exit if no files were selected
+    if (!filepaths) { return; }
+
+    jVideo.attr('src', filepaths[0]);
   };
 
 
   const askForSource = () => {
+    videoEl.pause();
     dialog.showOpenDialog( // open a file dialog, async returns list of paths
       {
         filters: [
--- app/Player/factories/askForSubs.js
+++ app/Player/factories/askForSubs.js
@@ -1,15 +1,19 @@
 const dialog = require('electron').remote.dialog;
 
 module.exports = (tags, services) => {
-
   const doubsService = services.doubtitles;
+  const videoEl = $(tags.video).get(0);
 
-  const changeSubs = (filepath) => {
-    doubsService.changeDoubs(filepath[0]);
+  const changeSubs = (filepaths) => {
+
+    // exit if no files were selected
+    if (!filepaths) { return; }
+
+    doubsService.changeDoubs(filepaths[0]);
   };
 
   const askForSubs = () => {
-
+    videoEl.pause();
     dialog.showOpenDialog( // open a file dialog, async returns list of paths
       {
         filters: [
